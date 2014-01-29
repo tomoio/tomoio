7a2f7226bdb757aabcfb38714a0fd6f2e7de3ae8
Tomoya Nagasawa <711@tomo.io>
2014-01-30 12:43:10 +1300

    add CNAME

--- Gruntfile.js
+++ Gruntfile.js
@@ -290,6 +290,7 @@ module.exports = function (grunt) {
                     dest: '<%= yeoman.dist %>',
                     src: [
                         '*.{ico,png,txt}',
+                        'CNAME',
                         '.htaccess',
                         'images/{,*/}*.webp',
                         '{,*/}*.html',
